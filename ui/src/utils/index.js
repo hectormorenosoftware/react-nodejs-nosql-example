cc2ba550e2cf85fd09f353ebfc89975e0df2906d
hectormorenosoftware <hectormorenosoftware@gmail.com>
2024-08-15 11:55:09 -0500

    Added keyframes for warnings

--- ui/src/utils/index.js
+++ ui/src/utils/index.js
@@ -5,7 +5,7 @@ export const emailRegexPattern = /^[a-zA-Z0-9]+@[a-z]+\.[a-z]{0,80}$/;
 export const slackRegexPattern = /^[a-zA-Z]{0,20}$/;
 export const companyRoleRegexPattern = /^[a-zA-z ]{0,80}$/;
 export const passwordRegexPattern = /^[a-zA-Z0-9]{0,20}$/;
-export const lastNameRegexPattern = /^[a-zA-Z0-9 ]{0,25}$/;
+export const lastNameRegexPattern = /^[a-zA-Z ]{0,40}$/;
 
 export function addASpace(str) {
   let response = str.replace(/([A-Z])/g, " $&");
