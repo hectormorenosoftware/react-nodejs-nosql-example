661d2fd77f1d329aed6821f0364d1609e34aab88
hectormorenosoftware <hectormorenosoftware@gmail.com>
2024-08-16 17:50:34 -0500

    Added new regex rules for lastnames and slack id

--- ui/src/utils/index.js
+++ ui/src/utils/index.js
@@ -2,10 +2,10 @@ export const stringRegexPattern = /^[a-zA-Z]{0,20}$/;
 export const phoneNumberRegexPattern = /^[0-9-]{0,24}$/;
 export const numberRegexPattern = /^[0-9]{0,7}$/;
 export const emailRegexPattern = /^[a-zA-Z0-9]+@[a-z]+\.[a-z]{0,80}$/;
-export const slackRegexPattern = /^[a-zA-Z]{0,20}$/;
+export const slackRegexPattern = /^[a-zA-Z ]{0,80}$/;
 export const companyRoleRegexPattern = /^[a-zA-z ]{0,80}$/;
 export const passwordRegexPattern = /^[a-zA-Z0-9]{0,20}$/;
-export const lastNameRegexPattern = /^[a-zA-Z ]{0,40}$/;
+export const lastNameRegexPattern = /^[a-zA-Z ]{0,80}$/;
 export const notesRegexPattern = /^[a-zA-Z0-9?() ]{0,10000}$/;
 
 export function addASpace(str) {
